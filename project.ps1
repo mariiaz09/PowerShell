4790c0f3dcb04213d204b32d28391d7279294e0f
mariiaz09 <142826683+mariiaz09@users.noreply.github.com>
2023-09-13 08:27:00 -0700

    final

--- project.ps1
+++ project.ps1
@@ -79,17 +79,69 @@ New-ADOrganizationalUnit -Name "NewOU" -Path "OU=ParentOU,DC=example,DC=com"
 #Create Groups 
 New-ADGroup -Name "MyGroup" -SamAccountName "MyGroup" -GroupCategory Security -GroupScope Global -DisplayName "My Group" -Path "OU=Groups,DC=example,DC=com"
 
-Create list of computers with a particular operating system installed 
+##Create list of computers with a particular operating system installed 
+#// Set CSV file name 
+    $uDateTime = Get-Date -f "yyyy-MM" 
+    $uCSVFile = "C:\Servers"+$uDateTime+".csv" 
 
-Create list of computers that have not logged onto the network within 30 days 
+    #//Export out to csv file.
+    Get-ADComputer -filter * -Properties ipv4Address, OperatingSystem,DistinguishedName | 
+    select-object Name, ipv4Address, OperatingSystem,  @{label='OU';expression= 
+    {$_.DistinguishedName.Split(',')[1].Split('=')[1]}} |
+    export-csv -path $uCSVFile
 
-Automatically remove items from Downloads folders 60+ days old 
 
-Create script to remote restart computer 
+##Create list of computers that have not logged onto the network within 30 days 
+$thresholdDate = (Get-Date).AddDays(-30)
 
-Retrieve disk size and amount of free space on a remote host 
+Get-ADComputer -Filter {LastLogonDate -lt $thresholdDate} -Property LastLogonDate | Select-Object Name, LastLogonDate
 
-Stop and start process on remote host 
+
+#Automatically remove items from Downloads folders 60+ days old 
+# Define the path to the Downloads folder
+$DownloadsFolder = [System.Environment]::GetFolderPath('Downloads')
+
+# Define the threshold date (60+ days ago)
+$ThresholdDate = (Get-Date).AddDays(-60)
+
+# Get all files in the Downloads folder that are older than the threshold date
+$OldFiles = Get-ChildItem -Path $DownloadsFolder | Where-Object { $_.LastWriteTime -lt $ThresholdDate }
+
+# Delete each old file
+foreach ($File in $OldFiles) {
+    Write-Host "Deleting $($File.Name)..."
+    Remove-Item -Path $File.FullName -Force
+}
+
+Write-Host "Cleanup completed."
+
+
+#Create script to remote restart computer
+Restart-Computer -ComputerName REMOTE_COMPUTER_NAME -Force
+
+#Retrieve disk size and amount of free space on a remote host 
+$disk = Get-WmiObject Win32_LogicalDisk -ComputerName remotecomputer -Filter "DeviceID='C:'" |
+Foreach-Object {$_.Size,$_.FreeSpace}
+
+##Stop and start process on remote host 
+# Remote computer name or IP address
+$RemoteComputer = "RemoteComputerNameOrIPAddress"
+
+# Process name to stop and start
+$ProcessName = "ProcessName"
+
+# Stop the process on the remote computer
+Invoke-Command -ComputerName $RemoteComputer -ScriptBlock {
+    param($ProcessName)
+    Stop-Process -Name $ProcessName -Force
+} -ArgumentList $ProcessName
+
+# Start the process on the remote computer
+Invoke-Command -ComputerName $RemoteComputer -ScriptBlock {
+    param($ProcessName)
+    Start-Process -Name $ProcessName
+} -ArgumentList $ProcessName
+
 
 #Stop and start services on remote host 
 Invoke-Command -ComputerName "RemoteComputer" -ScriptBlock {
